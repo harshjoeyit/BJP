a57a68c88683956d2dce529adb0fa9761e9c40df
Harshit Gangwar <harshit02gangwar@gmail.com>
2020-07-15 11:47:45 +0530

    folders managed

--- web/advanced_course/Advanced topics/ES2015.js
+++ web/advanced_course/Advanced topics/ES2015.js
@@ -10,14 +10,14 @@
 
 const arr = [1, 2, 3, 4];
 arr.push(5);
-console.log(arr);
+// console.log(arr);
 
 const colt = {
       name: 'Colt', 
       age: 12
 }
 colt.age = 15;
-console.log(colt);
+// console.log(colt);
 
 // with objects(and arrays) cannot be const 
 
@@ -41,7 +41,7 @@ function wish() {
       return msg;
       var msg = "Good Morning";
 }
-console.log(wish());
+// console.log(wish());
 
 // Above code looks like this actually 
 
@@ -83,7 +83,7 @@ function wishAgain() {
 
 var fName = "Harshit";
 var lName = "Gangwar";
-console.log(`Hello ${fName} ${lName}`);
+// console.log(`Hello ${fName} ${lName}`);
 
 
 
@@ -94,10 +94,10 @@ console.log(`Hello ${fName} ${lName}`);
 // if arrow function has only one argument then it does not need to put in ()
 
 var add = (a, b) => a+b;
-console.log(add(10, 15));
+// console.log(add(10, 15));
 
 var newArr = [1, 2, 3].map(value => value*2);
-console.log(newArr);
+// console.log(newArr);
 
 
 // double and filter divisible by 3
@@ -112,8 +112,8 @@ var doubleAndFilter = function(arr) {
 
 var niceDoubleAndFilter = arr => arr.map(val => val*2).filter(num => num %3 === 0);
 
-console.log(doubleAndFilter([3, 5, 2, 1, 9]));
-console.log(niceDoubleAndFilter([3, 5, 2, 1, 9]));
+// console.log(doubleAndFilter([3, 5, 2, 1, 9]));
+// console.log(niceDoubleAndFilter([3, 5, 2, 1, 9]));
 
 
 // this same example used in call, bind, apply
@@ -123,7 +123,7 @@ var instructor = {
       sayHi: function() {
             setTimeout(() => {
                   // this refers to the enclosing context of function i.e. object itself  
-                  console.log("Hello " + this.fName);
+                  // console.log("Hello " + this.fName);
             }, 1000);
             // setTimeout is the function of window object so if the arrow function 
             // is not used then this referes to the window object and not instructor object
@@ -173,9 +173,9 @@ var names = [
 
 // fiilter names that have length < 5 and put 
 // them in dectionary with name and length as kwy value pair
-console.log(mapFilterAndReduce(names));
+// console.log(mapFilterAndReduce(names));
 
-console.log(niceMapFilterAndReduce(names));
+// console.log(niceMapFilterAndReduce(names));
 
 
 let Student = function(name, age) {
@@ -185,12 +185,12 @@ let Student = function(name, age) {
 }
 
 var ramesh = new Student("Ramesh", 15);
-console.log(ramesh);
+// console.log(ramesh);
 
 let niceStudent = (name, age) => ({ name: name, age: age, college: "MNNIT" })
 
 var suresh = new Student("Suresh", 13);
-console.log(suresh);
+// console.log(suresh);
 
 
 
@@ -201,16 +201,16 @@ console.log(suresh);
 function add(a, b) {
       return a+b;
 }
-console.log(add());
+// console.log(add());
 // prints NaN
 
 function mul(a=3, b=4) {
       return a*b;
 }
 
-console.log(mul())
-console.log(mul(5));
-console.log(mul(7, 8));
+// console.log(mul())
+// console.log(mul(5));
+// console.log(mul(7, 8));
 
 
 
@@ -229,9 +229,9 @@ for(let num of a) {
 // Rest operator 
 
 function printArgs(a, b, ...c) {
-      console.log(a);
-      console.log(b);
-      console.log(c);
+      // console.log(a);
+      // console.log(b);
+      // console.log(c);
 }
 
 printArgs(1);
@@ -260,9 +260,9 @@ var niceSumArgs = (...args) => args.reduce((acc, next) => acc + next);
 // we dont need  to explicitly convert it, 
 // we can simply use reduce on it 
 
-console.log(sumArgs(1, 2, 3, 4));
-console.log(anotherSumArgs(1, 2, 3, 4));
-console.log(niceSumArgs(1, 2, 3, 4)); 
+// console.log(sumArgs(1, 2, 3, 4));
+// console.log(anotherSumArgs(1, 2, 3, 4));
+// console.log(niceSumArgs(1, 2, 3, 4)); 
 
 
 
@@ -274,11 +274,11 @@ var arr2 = [1, 2, 3];
 var arr3 = [1, 2 ,3];
 
 a = [...arr1, ...arr2, ...arr3];
-console.log(a);
+// console.log(a);
 
 // Math.max operator does not work on arrays - so we use apply 
 // or the spread operator 
-console.log(Math.max(...a));
+// console.log(Math.max(...a));
 
 
 function sum(a, b, c) {
@@ -286,7 +286,127 @@ function sum(a, b, c) {
 }
 
 a = [1, 2, 3];
-console.log(sum(...a));
+// console.log(sum(...a));
 
 
 
+// finding minmum from the array 
+function smallest(...args) {
+      // first recieve the array as an argument using the
+      // Rest operator 
+      // Use Spread operator since the max deos not accept the array 
+      return Math.min(...args);
+}
+
+// console.log(smallest(1, 2, -1, 3));
+
+
+// concatinating array;
+function joinArrays(...args) {
+      return args.reduce((acc, next) => acc.concat(next), []);
+}
+// console.log(joinArrays([1, 2, 3], [3, 4, 5], [1, 2], [1]));
+
+
+
+
+
+
+// Object Enhancement
+
+var fName = "Rohit";
+var lName = "Mehra";
+
+var person1 = {
+      fName: fName, 
+      lName: lName
+}
+// console.log(person1);
+
+// if keys and values are same, then this is a shorthand
+var person2 = {
+      fName, 
+      lName
+}
+// console.log(person2)
+
+
+var obj1 = {
+      callout: function() {
+            return "Hello";
+      }
+}
+// ES2015
+var obj2 = {
+      callout() {
+            return "Hi";
+      }
+}
+// console.log(obj1.callout());
+// console.log(obj2.callout());
+
+
+
+// Destructuring
+// extracting values data storedin arrays and objects
+
+
+
+var person = {
+      firstName: "Harshit",
+      lastName: "Gangwar"
+}
+// unpack
+var {firstName, lastName} = person
+// console.log(firstName, lastName);
+
+// our new variable name
+var {firstName:first, lastName:last} = person;
+// console.log(first, last);
+
+
+// before ES2015
+function displayInfo(obj) {
+      return [obj.name, obj.favColor];
+}
+// destructured object as a parameter 
+function niceDisplayInfo({name, favColor}) {
+      return [name, favColor];
+}
+
+var obj = {name: "Elie", favColor: "Yellow"}
+// console.log(displayInfo(obj));
+// console.log(niceDisplayInfo(obj));
+
+
+// Array destructuring
+
+a = [1, 2, 3, 4];
+var [a, b, c, d] = a;
+// console.log(a, b, c, d);
+
+function returnNums(a, b) {
+      return [a, b];
+}
+
+[first, second] = returnNums(3, 4);
+// console.log(first, second);
+
+
+// swapping 
+function swap(a, b) {
+      return [a, b] = [b, a];
+}
+// console.log(swap(5, 19));
+
+
+// reversing an array
+function reverse(a) {
+      for(var i=0; i<a.length/2; i++) {
+            a[a[i], a[a.length-i-1]] = [a[a.length-1-i], a[i]];
+      }
+      return a;
+}
+
+a =[1, 2, 3, 4, 5];
+// console.log(reverse(a));
