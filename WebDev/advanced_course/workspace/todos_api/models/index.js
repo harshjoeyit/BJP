c7e242c885cef4164d8639d73067eb22b0e802b2
harshjoeyit <harshit02gangwar@gmail.com>
2020-11-03 14:31:29 +0530

    folders changed

--- WebDev/advanced_course/workspace/todos_api/models/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
-
-mongoose.Promise = Promise;
-
-module.exports.Todo = require("./todo");
